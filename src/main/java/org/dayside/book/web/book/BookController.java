f783920c90788af50b689a429363f6e13429f2da
devson <sonyc5720@gmail.com>
2019-01-19 13:12:39 +0900

    책검색 view 용 메서드 : model에 책 검색 결과를 담는 코드 추가

--- src/main/java/org/dayside/book/web/book/BookController.java
+++ src/main/java/org/dayside/book/web/book/BookController.java
@@ -1,14 +1,20 @@
 package org.dayside.book.web.book;
 
-import org.dayside.book.web.book.model.BookApiResultModel;
+import org.apache.commons.lang3.StringUtils;
+import org.dayside.book.web.book.model.NaverBookApiSearchResultModel;
+import org.dayside.book.web.book.model.BookSearchModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/books")
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
 public class BookController {
     private BookService bookService;
 
@@ -16,8 +22,25 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @GetMapping
-    public ResponseEntity<BookApiResultModel> getBooks(@RequestParam String keyword, @RequestParam(defaultValue = "1") int page) {
-        return bookService.getBooks(keyword, page);
+    @GetMapping("/search")
+    public String bookSearchView(String keyword, @RequestParam(defaultValue = "1") int page,
+                           Model model) {
+        if (StringUtils.isBlank(keyword)) {
+            return "book-search";
+        }
+
+        ResponseEntity<NaverBookApiSearchResultModel> booksResult = bookService.getBooks(keyword, page);
+
+        if (booksResult.getStatusCode() != HttpStatus.OK) {
+            return "error"; // TODO error page 만들기
+        }
+
+        List<BookSearchModel> bookList = booksResult.getBody().getItems();
+
+        if (bookList != null && !bookList.isEmpty()) {
+            model.addAttribute("bookList", bookList);
+        }
+
+        return "book-search";
     }
 }
