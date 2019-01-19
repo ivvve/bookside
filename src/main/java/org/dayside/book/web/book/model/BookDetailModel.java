1b4256f31168a85866fbd22d98fc04c51bdbafc0
devson <sonyc5720@gmail.com>
2019-01-20 01:50:51 +0900

    image uri의 query string 제거

--- src/main/java/org/dayside/book/web/book/model/BookDetailModel.java
+++ src/main/java/org/dayside/book/web/book/model/BookDetailModel.java
@@ -1,6 +1,7 @@
 package org.dayside.book.web.book.model;
 
 import lombok.Data;
+import org.dayside.book.util.BookUtils;
 
 @Data
 public class BookDetailModel {
@@ -14,4 +15,14 @@ public class BookDetailModel {
     private String pubdate;
     private String isbn;
     private String description;
+
+    /**
+     * api 검색 결과에서 image(책의 표지 이미지 주소)는 query string을 붙여서 제공하는데
+     * 이를 그대로 사용하면 책의 표지 이미지가 너무 작기 때문에
+     * query string을 제거한 image url로 지정한다.
+     * @param image
+     */
+    public void setImage(String image) {
+        this.image = BookUtils.removeQueryStringFromUri(image);
+    }
 }
