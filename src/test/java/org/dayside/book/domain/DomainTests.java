d4546251963769425a8da39ad8dcf15f71cc1329
devson <sonyc5720@gmail.com>
2019-01-23 18:24:55 +0900

    도메인 설계 변경으로 인한 테스트 변경 (테스트를 어떻게 짜야할까...)

--- src/test/java/org/dayside/book/domain/DomainTests.java
+++ src/test/java/org/dayside/book/domain/DomainTests.java
@@ -3,15 +3,19 @@ package org.dayside.book.domain;
 import org.dayside.book.domain.enums.BookOrderStatus;
 import org.dayside.book.domain.enums.MemberStatus;
 import org.dayside.book.repository.BookOrderRepository;
+import org.dayside.book.repository.BookRepository;
 import org.dayside.book.repository.MemberRepository;
 import org.hibernate.Session;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -23,6 +27,7 @@ import static org.hamcrest.Matchers.is;
 public class DomainTests {
     @Autowired MemberRepository memberRepository;
     @Autowired BookOrderRepository bookOrderRepository;
+    @Autowired BookRepository bookRepository;
 
     private final String TESTER_EMAIL = "tester@gmail.com";
     private final String TESTER_PASSWORD = "test1234!@#$";
@@ -32,46 +37,24 @@ public class DomainTests {
     private final int BOOK_PRICE = 67500;
     private final String ORDER_ADDRESS = "서울특별시";
 
-    @Autowired
-    EntityManager entityManager;
-    @Test
-    public void 회원_책주문_매핑_테스트() {
-        Session session = entityManager.unwrap(Session.class);
-
-        MemberEntity member = MemberEntity.builder()
-                .email(TESTER_EMAIL)
-                .password(TESTER_PASSWORD)
-                .memberStatus(MemberStatus.ADMIN)
+    @Test(expected = DataIntegrityViolationException.class)
+    public void 주문자가_없는_경우_주문_저장_오류_테스트() {
+        BookEntity book = BookEntity.builder()
+                .isbn(BOOK_ISBN)
+                .title(BOOK_TITLE)
+                .price(BOOK_PRICE)
                 .build();
+
+        bookRepository.save(book);
 
         BookOrderEntity bookOrder = BookOrderEntity.builder()
-                .book(new Book(BOOK_TITLE, BOOK_ISBN, BOOK_PRICE))
+                .orderStatus(BookOrderStatus.REQUEST)
                 .address(ORDER_ADDRESS)
                 .orderDate(LocalDateTime.now())
-                .orderStatus(BookOrderStatus.REQUEST)
-                .orderer(member)
+                .updatedDate(LocalDateTime.now())
                 .build();
+        bookOrder.addBook(book);
 
-        member.addBookOrder(bookOrder);
-
-
-        memberRepository.save(member);
         bookOrderRepository.save(bookOrder);
-
-        session.flush();
-
-        member = memberRepository.getOne(member.getId());
-        bookOrder = bookOrderRepository.getOne(bookOrder.getId());
-
-        assertThat(bookOrder.getOrderer(), is(member));
-        assertThat(member.getBookOrderSet(), contains(bookOrder));
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void 주문자가_없는_경우_주문_저장_오류_테스트() {
-        bookOrderRepository.save(BookOrderEntity.builder()
-                .book(new Book(BOOK_TITLE, BOOK_ISBN, BOOK_PRICE))
-                .address(ORDER_ADDRESS)
-                .build());
     }
 }
