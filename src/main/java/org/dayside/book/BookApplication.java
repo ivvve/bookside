7bd75bc3a7b7c3fd35a7e3c396a954579442d63d
devson <sonyc5720@gmail.com>
2019-02-10 01:01:22 +0900

    프로젝트 갈아 엎음

--- src/main/java/org/dayside/book/BookApplication.java
+++ src/main/java/org/dayside/book/BookApplication.java
@@ -1,27 +1,13 @@
 package org.dayside.book;
 
-import org.dayside.book.domain.MemberEntity;
-import org.dayside.book.domain.enums.MemberStatus;
-import org.dayside.book.repository.MemberRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(MemberRepository memberRepository) {
-        return arg -> memberRepository.save(MemberEntity.builder()
-                .email("sonyc5720@gmail.com")
-                .password("hello1234")
-                .memberStatus(MemberStatus.ADMIN)
-                .build());
     }
 }
 
