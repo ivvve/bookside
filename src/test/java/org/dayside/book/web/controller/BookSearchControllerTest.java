76fe7f0a0753b85c4ce6d3644e1b6a4a19495bc3
devson <sonyc5720@gmail.com>
2019-02-12 22:22:08 +0900

    API 호출 결과에 따라 view에 표시되는 텍스트를 확인하는 테스트코드 추가

--- src/test/java/org/dayside/book/web/controller/BookSearchControllerTest.java
+++ src/test/java/org/dayside/book/web/controller/BookSearchControllerTest.java
@@ -8,16 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
+import java.util.Collections;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BookSearchController.class)
@@ -49,5 +51,34 @@ public class BookSearchControllerTest {
                 .andDo(print())
                 .andExpect(view().name("search/search"))
                 .andExpect(model().attributeExists("bookList"));
+    }
+
+    @Test
+    public void API_호출_오류의_경우_view_테스트() throws Exception {
+        when(bookSearchService.searchBooks("헬로 월드", "title", 1))
+                .thenReturn(new PageImpl<>(Collections.emptyList(), PageRequest.of(0, 1), 0));
+
+        mockMvc.perform(get("/search")
+                .param("keyword", "헬로 월드")
+                .param("target", "title")
+                .param("page", "1"))
+                .andDo(print())
+                .andExpect(view().name("search/search"))
+                .andExpect(content().string(containsString("서버에 문제가 생겼습니다.")))
+                .andExpect(content().string(containsString("다시 시도해주세요.")));
+    }
+
+    @Test
+    public void 책_검색_결과_없는_경우_view_테스트() throws Exception {
+        when(bookSearchService.searchBooks("헬로 월드", "title", 1))
+                .thenReturn(new PageImpl<>(Collections.emptyList(), PageRequest.of(1, 12), 0));
+
+        mockMvc.perform(get("/search")
+                .param("keyword", "헬로 월드")
+                .param("target", "title")
+                .param("page", "1"))
+                .andDo(print())
+                .andExpect(view().name("search/search"))
+                .andExpect(content().string(containsString("검색 결과가 없습니다.")));
     }
 }
